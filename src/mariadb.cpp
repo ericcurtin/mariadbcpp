3ffd6853340e3ed8d411a00053ea0b9d78f012ed
Eric Curtin <ericcurtin17@gmail.com>
2017-10-04 20:00:45 +0100

    Connect to DB and execute query
    Also methods for getSQLState and getError if something goes wrong

--- src/mariadb.cpp
+++ src/mariadb.cpp
@@ -1,9 +1,39 @@
 #include "mariadb.hpp"
 
+#include <string.h>
+
+using std::string;
+
 mariadbcpp::mariadbcpp() {
   mysql = mysql_init(0);
 }
 
 mariadbcpp::~mariadbcpp() {
   mysql_close(mysql);
+}
+
+bool mariadbcpp::connect(const char* host,
+                         const char* user,
+                         const char* pass,
+                         const char* db) {
+  return mysql_real_connect(mysql,
+                            host,
+                            user,
+                            pass,
+                            db,
+                            0,
+                            "/tmp/mysql.sock",
+                            0);
+}
+
+int mariadbcpp::query(const char* query) {
+  return mysql_real_query(mysql, query, strlen(query));
+}
+
+string mariadbcpp::getSQLState() {
+  return mysql_sqlstate(mysql);
+}
+
+string mariadbcpp::getError() {
+  return mysql_error(mysql);
 }
--- src/mariadb.hpp
+++ src/mariadb.hpp
@@ -1,9 +1,17 @@
 #include <mariadb/mysql.h>
+#include <string>
 
 class mariadbcpp {
   public:
     mariadbcpp();
     ~mariadbcpp();
+    bool connect(const char* host,
+                 const char* user,
+                 const char* pass,
+                 const char* db);
+    int query(const char* query);
+    std::string getSQLState();
+    std::string getError();
   private:
-    static MYSQL* mysql;
+    MYSQL* mysql;
 };
